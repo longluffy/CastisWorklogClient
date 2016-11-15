30db5534235ef9f40029639fbdf21adeaa31e018
longluffy <longnd2791@gmail.com>
2016-11-15 19:22:17 +0700

    commit test

--- app/src/main/java/com/castis/castisworklogclient/View/SettingActivity.java
+++ app/src/main/java/com/castis/castisworklogclient/View/SettingActivity.java
@@ -1,31 +1,9 @@
 package com.castis.castisworklogclient.View;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.castis.castisworklogclient.Presenter.HttpHandler;
 import com.castis.castisworklogclient.R;
-import com.castis.castisworklogclient.model.User;
-import com.google.gson.Gson;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 public class SettingActivity extends PreferenceActivity {
     @Override
