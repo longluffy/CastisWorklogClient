05e68f05fe18a59cc1290c14a66ab317297ebddd
longluffy <longnd2791@gmail.com>
2016-11-17 18:01:09 +0700

    commit test

--- app/src/main/java/com/castis/castisworklogclient/model/User.java
+++ app/src/main/java/com/castis/castisworklogclient/model/User.java
@@ -15,10 +15,6 @@ public class User
 
     private Integer status;
 
-    private Team team;
-
-    private Role role;
-
 
     public String getUsername() {
         return username;
@@ -60,22 +56,6 @@ public class User
         this.status = status;
     }
 
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
     public int getId() {
         return id;
     }
@@ -84,68 +64,5 @@ public class User
         this.id = id;
     }
 
-    public class Team {
-        private int team_id;
-
-        private String team_name;
-
-        private String project;
-
-        private int status;
-
-        public int getTeam_id() {
-            return team_id;
-        }
-
-        public void setTeam_id(int team_id) {
-            this.team_id = team_id;
-        }
-
-        public String getTeam_name() {
-            return team_name;
-        }
-
-        public void setTeam_name(String team_name) {
-            this.team_name = team_name;
-        }
-
-        public String getProject() {
-            return project;
-        }
-
-        public void setProject(String project) {
-            this.project = project;
-        }
-
-        public int getStatus() {
-            return status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-    }
-
-    public class Role {
-        private int role_id;
-
-        private String role;
-
-        public String getRole() {
-            return role;
-        }
-
-        public void setRole(String role) {
-            this.role = role;
-        }
-
-        public int getRole_id() {
-            return role_id;
-        }
-
-        public void setRole_id(int role_id) {
-            this.role_id = role_id;
-        }
-    }
 
 }
